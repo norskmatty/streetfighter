1ee3ae5ed6cd55b9398521ea8e3bec7c00089e3f
norskmatty <matthewconley00@gmail.com>
2016-05-23 14:23:05 -0700

    challenge

--- js/app.js
+++ js/app.js
@@ -13,19 +13,31 @@ $(document).ready(function() {
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show().animate( 
 			{'left': '1020px'},
-			500,
+			800,
 			function() {
 				$(this).hide();
 				$(this).css('left', '520px');
 			}
-	);
-		// play hadouken sound
-		// show hadouken and animate it to the right of the screen		
+		);
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 		// ryu goes back to his ready position
+	});
+
+$(document).keydown(function(e) {
+		if (e.keyCode = 88) {
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		}	
+	})
+	.keyup(function(e) {
+		if (e.keyCode = 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		}
 	});
 });
 
