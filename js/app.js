9a90ac282a96ed71dd542961b3b402bd2bd838b7
norskmatty <matthewconley00@gmail.com>
2016-05-23 15:40:38 -0700

    with added effects

--- js/app.js
+++ js/app.js
@@ -19,6 +19,12 @@ $(document).ready(function() {
 				$(this).css('left', '520px');
 			}
 		);
+		$('.hulk-ryu-ow').delay(790).queue(function(n) {
+			$(this).show(); n();
+		});
+		$('.hulk-ryu-ow').delay(1000).queue(function(o) {
+			$(this).hide(); n();
+		});
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
@@ -26,17 +32,64 @@ $(document).ready(function() {
 		// ryu goes back to his ready position
 	});
 
-$(document).keydown(function(e) {
-		if (e.keyCode = 88) {
+	$('.hulk-ryu').mouseenter(function() {
+		$('.hulk-ryu-still').hide();
+		$('.hulk-ryu-ready').show();
+	})
+	.mouseleave(function() {
+		$('.hulk-ryu-still').show();
+		$('.hulk-ryu-ready').hide();
+	})
+	.mousedown(function() {
+		playHadouken();
+		$('.hulk-ryu-ready').hide();
+		$('.hulk-ryu-throwing').show();
+		$('.hulk-hadouken').finish().show().animate( 
+			{'left': '335px'},
+			800,
+			function() {
+				$(this).hide();
+				$(this).css('left', '865px');
+			}
+		);
+		$('.ryu-ow').delay(790).queue(function(n) {
+			$(this).show(); n();
+		});
+		$('.ryu-ow').delay(1000).queue(function(n) {
+			$(this).hide(); n();
+		});
+	})
+	.mouseup(function() {
+		$('.hulk-ryu-throwing').hide();
+		$('.hulk-ryu-ready').show();
+		// ryu goes back to his ready position
+	});
+
+	$(document).keydown(function(e) {
+		if (e.keyCode == 88) {
 			$('.ryu-still').hide();
 			$('.ryu-ready').hide();
 			$('.ryu-cool').show();
 		}	
 	})
 	.keyup(function(e) {
-		if (e.keyCode = 88) {
+		if (e.keyCode == 88) {
 			$('.ryu-cool').hide();
 			$('.ryu-still').show();
+		}
+	});
+
+	$(document).keydown(function(e) {
+		if (e.keyCode == 89) {
+			$('.hulk-ryu-still').hide();
+			$('.hulk-ryu-ready').hide();
+			$('.hulk-ryu-cool').show();
+		}	
+	})
+	.keyup(function(e) {
+		if (e.keyCode == 89) {
+			$('.hulk-ryu-cool').hide();
+			$('.hulk-ryu-still').show();
 		}
 	});
 });
